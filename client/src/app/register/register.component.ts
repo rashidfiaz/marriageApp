b873f7c47cccca356a453a5114d752d080b48202
rashidfiaz <90270550+rashidfiaz@users.noreply.github.com>
2024-12-19 18:02:59 -0500

    End of Section 5

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  
+  accountService = inject(AccountService);
+  
+  //getUsersFromtheHomeComponent = input.required<any>();
+  cancelRegister = output<boolean>();
+  model: any = {};
+
+  registerUser() {
+    this.accountService.registerUser(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
+     
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
